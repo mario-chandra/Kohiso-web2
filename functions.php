621c15cf84b85159a1e7baa5cad46660da944258
mario-chandra <mariochandra020@gmail.com>
2020-07-14 23:46:49 +0700

    menambahkan tampilan item

--- functions.php
+++ functions.php
@@ -20,4 +20,14 @@
     return mysqli_affected_rows($conn);
   }
 
+  function fetchData($query){
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $datas = [];
+    while ($data = mysqli_fetch_assoc($result)) {
+      $datas[] = $data;
+    }
+    return $datas;
+  }
+
  ?>
