aa30e2fb50b46b64b38bf9efa01c405f68f60fb4
mario-chandra <mariochandra020@gmail.com>
2020-07-15 23:22:28 +0700

    penambahan id di database

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 
     $query = "INSERT INTO item
     VALUES (
-      '$name' , '$deskripsi' , '$harga'
+     '',  '$name' , '$deskripsi' , '$harga'
     ) ";
 
     mysqli_query($conn,$query);
@@ -32,9 +32,27 @@
 
   function deleteData($data){
    global $conn;
-   mysqli_query($conn,"DELETE FROM item WHERE name = '$data'");
+   mysqli_query($conn,"DELETE FROM item WHERE id = '$data'");
    return mysqli_affected_rows($conn);
  }
 
+  function updateData($data){
+    global $conn;
+    $id = $data["id"];
+    $name = $data["name"];
+    $deskripsi = $data["deskripsi"];
+    $harga = $data["harga"];
+
+    $query = "UPDATE item SET
+      name ='$name' ,
+      deskripsi = '$deskripsi' ,
+      harga = '$harga'
+      WHERE id = $id";
+
+
+    mysqli_query($conn,$query);
+
+    return mysqli_affected_rows($conn);
+  }
 
  ?>
---
+++ gantiitem.php
@@ -0,0 +1,51 @@
+<?php
+  require 'functions.php';
+
+
+  $id = $_GET["id"];
+
+
+  $item = fetchData("SELECT * FROM item WHERE id = '$id'")[0];
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>EDIT ITEM</h1>
+     <form class="" method="post">
+       <input type="hidden" name="id" value="<?= $item["id"] ?>">
+       <label for="name">Name:</label>
+       <input type="text" name="name" id="name" value="<?= $item["name"] ?>">
+       <br>
+       <label for="deskripsi">deskripsi:</label>
+       <input type="text" name="deskripsi" id="deskripsi" value="<?= $item["deskripsi"] ?>">
+       <br>
+       <label for="harga">harga</label>
+       <input type="text" name="harga" id="harga" value="<?= $item["harga"] ?>">
+       <br>
+       <button type="submit" name="submit">submit</button>
+     </form>
+
+     <?php
+     if(isset($_POST["submit"])):
+       if(updateData($_POST)>0):
+         echo "<script>
+              alert('berhasil');
+              document.location.href = 'tampilan.php';
+              </script>";
+       else :
+         echo "gagal";
+       endif;
+     endif;
+      ?>
+
+      <a href="tampilan.php">kembali</a>
+
+
+   </body>
+ </html>
