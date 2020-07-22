51b99c772bd82c38d6486e61243c57d6346f10e6
StevenTannn <Steven.tanoto00@gmail.com>
2020-07-22 16:51:25 +0700

    update data

--- setting_metatag.php
+++ setting_metatag.php
@@ -11,7 +11,7 @@ include "security.php";
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <!-- Custom styles template ini-->
         <link href="asset/style/style_admin.css" rel="stylesheet" type="text/css" media="all">
         <!-- Custom Fonts Awesome-->
