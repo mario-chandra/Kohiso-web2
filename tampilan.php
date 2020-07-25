b9d416ef4441f88482418d8669d105c0025973fb
StevenTannn <Steven.tanoto00@gmail.com>
2020-07-25 20:20:15 +0700

    update

--- tampilan.php
+++ tampilan.php
@@ -80,7 +80,9 @@
     <?php endforeach; ?>
         </tbody>
     </table>
+            </div>
+           </div>
+       </div>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
-
   </body>
 </html>
