2653e28f4644237988e8869b00f7a788215926ee
StevenTannn <Steven.tanoto00@gmail.com>
2020-07-25 21:38:13 +0700

    update andika

--- tampilan.php
+++ tampilan.php
@@ -80,9 +80,14 @@
     <?php endforeach; ?>
         </tbody>
     </table>
+        <<<<<<< master
+   imgview
+=======
+    </table>
             </div>
            </div>
        </div>
+>>>>>>> master
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
   </body>
 </html>
