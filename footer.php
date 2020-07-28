25eff1b80cc4b75dccb54c94c882bc081d98062e
StevenTannn <steven.tanoto00@gmail.com>
2020-07-28 22:23:45 +0700

    membuat footer lebih dinamis

---
+++ footer.php
@@ -0,0 +1,28 @@
+<div class="footer">
+        <div class="row footer">
+            <div class="col-4 font-weight-bold">
+                <a href=index.php class="footer-link">HOME</a>
+                <a href=shop.php class="footer-link">SHOP</a>
+                <a href=press.php class="footer-link">PRESS</a>
+                <a href=us.php class="footer-link">ABOUT US</a>
+            </div>
+            <div class="col-8">
+                <p class="font-weight-bold">Subcription</p>
+                <p>Never run out of your favorite coffees, teas and powders again with our auto-delivery subscription. Select how often your products arrive, pause and cancel anytime</p>
+                <div class="row">
+                    <div class="col-5">
+                        <input class="form-control">
+                    </div>
+                    <div class="col-4 pr-0">
+                        <button class="btn kohiso-btn purple-bg py-2 px-3">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-5 text-black text-center">
+            <a class="navbar-brand" href="/index.html">
+                <img src="asset/img/Logo.png" class='logo'>
+            </a>
+        </div>
+    </div>
--- signup.php
+++ signup.php
@@ -104,35 +104,9 @@
 
     </div>
 
-    <div class="footer">
-        <div class="row footer">
-            <div class="col-4 font-weight-bold">
-                <a href=index.html class="footer-link">HOME</a>
-                <a href=shop.html class="footer-link">SHOP</a>
-                <a href=press.html class="footer-link">PRESS</a>
-                <a href=us.html class="footer-link">ABOUT US</a>
-                <a href=contact.html class="footer-link">CONTACT US</a>
-            </div>
-            <div class="col-8">
-                <p class="font-weight-bold">Subcription</p>
-                <p>Never run out of your favorite coffees, teas and powders again with our auto-delivery subscription. Select how often your products arrive, pause and cancel anytime</p>
-                <div class="row">
-                    <div class="col-5">
-                        <input class="form-control">
-                    </div>
-                    <div class="col-4 pr-0">
-                        <button class="btn kohiso-btn purple-bg py-2 px-3">Submit</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-5 text-black text-center">
-            <a class="navbar-brand" href="/index.html">
-                <img src="asset/img/Logo.png" class='logo'>
-            </a>
-        </div>
-    </div>
+    <?php
+        require "footer.php";
+    ?>
 
     <script src="asset/script/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
